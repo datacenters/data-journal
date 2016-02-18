f2ca2e43d035c303852dcc5dcc8c32a9316de81a
Paul Mylecharane <hi@public-office.info>
2016-02-18 17:06:54 +1100

    shifting content

--- site/templates/home.php
+++ site/templates/home.php
@@ -1,17 +1,10 @@
 <?php snippet('header') ?>
 
   <div class="home-left" role="main">
-    <!-- <section class="schrift-text">
-      <h2><?php echo $page->schrift()->html(); ?></h2>
-    </section>
-    <nav>
-      <li><a>Type Designers</a><small>(<?php echo $pages->find('interviews')->children()->visible()->count(); ?>)</small></li>
-      <li><a>Type Foundries</a><small></small></li>
-      <li><a>Typographers</a><small></small></li>
-    </nav> -->
 
-    <h1>AU Data Sovereignty</h1>
+    <h1>Data Journal</h1>
 
+    <?php echo $page->text()->kirbytext() ?>
     <div class="content-list">
       <ul>
         <?php foreach(page('interviews')->children()->visible()->limit(5)->offset(0) as $interview): ?>
@@ -36,30 +29,9 @@
   </div>
 
   <div class="home-right" role="main">
-
-    <!-- <div class="client-list art">
-      <ul>
-        <?php foreach(page('interviews')->children()->visible()->limit(5)->offset(0) as $interview): ?>
-        <li>
-          <small><?php echo $interview->published(); ?></small>
-          <a href="<?php echo $interview->url() ?>"><?php echo html($interview->title()) ?></a>
-        </li>
-        <?php endforeach ?>
-      </ul>
-    </div>
-
-    <div class="client-list art">
-      <ul>
-        <?php foreach(page('interviews')->children()->visible()->offset(5)->limit(5) as $interview): ?>
-        <li>
-          <small><?php echo $interview->published(); ?></small>
-          <a href="<?php echo $interview->url() ?>"><?php echo html($interview->title()) ?></a>
-        </li>
-        <?php endforeach ?>
-      </ul>
-    </div> -->
-
-
+    <figure>
+      <img src="<?php echo url('assets/images/Dymaxion2.jpg') ?>">
+    </figure>
   </div>
 
 <?php snippet('footer') ?>
--- site/templates/interview.php
+++ site/templates/interview.php
@@ -17,9 +17,16 @@
   </div>
 
   <div class="right" role="main">
-
-
-
+    <div class="inner">
+      <ul>
+        <li>Related Content</li>
+        <li><a href="#">Plain</a></li>
+        <li><a href="#">Theinhardt</a></li>
+        <li><a href="#">Genath</a></li>
+        <li><a href="#">Clarendon Graphic</a></li>
+        <li><a href="#">Didot Elder</a></li>
+      </ul>
+    </div>
   </div>
 
 <?php snippet('footer'); ?>
